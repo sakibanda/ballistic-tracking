20696395cd5ac33474c153e69b152d1354d41f7b
barcelona23 <me@adriancr.com>
2013-10-28 23:31:36 +0000

    150 ticket

--- app/private/includes/reporting/stats.php
+++ app/private/includes/reporting/stats.php
@@ -48,6 +48,7 @@ function runStats($user_pref){
     DB::query($sql);
     /** END CLICK DATA **/
 
+
     DB::query("update bt_c_statcache c
             left join (select
                     meta1,
@@ -57,11 +58,12 @@ function runStats($user_pref){
                     (sum(s.income) / sum(s.clicks)) as payout,
                     sum(s.income) as income
                     from bt_c_statcache s
+                    where type='stats'
                   ) data
             on data.meta1 > 0
             set
                 c.clicks=data.clicks, c.click_throughs=data.click_throughs, c.click_through_rates=(c.click_throughs / c.clicks), c.leads=data.leads, c.conv=(c.leads / c.clicks), c.payout=data.payout, c.income=data.income
-                where (c.meta3 is null or c.meta3=0)");
+                where (c.meta3 is null or c.meta3=0) AND type='stats' ");
 
     reCalculateIncomes($incomes);
     calculateCosts($spends);
@@ -107,7 +109,7 @@ function calculateCosts($spends) {
 				cpc='" . DB::quote($row['cpc']) . "',
 				net='" . DB::quote($row['net']) . "',
 				roi='" . DB::quote($row['roi']) . "'
-				where id='" . DB::quote($row['id']) . "'");
+				where id='" . DB::quote($row['id']) . "' and type='stats' ");
 
         //if it has a campaign, update the top lvl LP as well
         if($row['meta2']) {
