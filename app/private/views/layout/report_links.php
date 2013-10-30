cfdc1c7dfa44a53b445a14104a62d15b77f84302
barcelona23 <me@adriancr.com>
2013-10-30 10:33:31 +0000

    143 ticket

--- app/private/views/layout/report_links.php
+++ app/private/views/layout/report_links.php
@@ -1,7 +1,6 @@
 <div class="grid_12" style="text-align: center;">
     <a href="/reports/">Custom Data Report</a> -
     <a href="/reports/breakdown">Breakdown</a> -
-    <a href="/dateparting/">Date Parting</a> -
     <a href="/platforms/mobile">Mobile Breakdown</a> -
     <a href="#">Subid Analysis Report</a>
 </div>
---
+++ app/private/views/layout/stats_links.php
@@ -0,0 +1,4 @@
+<div class="grid_12" style="text-align: center;">
+    <a href="/stats/">Campaign Stats</a> -
+    <a href="/dateparting/">Week/Date Parting</a>
+</div>
